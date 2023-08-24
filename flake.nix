6bdeae96bc8adc28b3a55a5f056960cb89077c2e
Dirli-V <github@dirli.net>
2023-08-24 19:49:34 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Flake for LogiNabe";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      craneLib = crane.lib.${system};
+      loginabe = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        # buildInputs = [
+        #   # Add additional build inputs here
+        # ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        #   # Additional darwin specific inputs can be set here
+        #   pkgs.libiconv
+        # ];
+
+        # Additional environment variables can be set directly
+        # MY_CUSTOM_VAR = "some value";
+      };
+    in {
+      checks = {
+        inherit loginabe;
+      };
+
+      packages.default = loginabe;
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = loginabe;
+      };
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = builtins.attrValues self.checks.${system};
+
+        # Additional dev-shell environment variables can be set directly
+        # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+        # Extra inputs can be added here
+        # nativeBuildInputs = with pkgs; [
+        #   cargo
+        #   rustc
+        # ];
+      };
+    });
+}
