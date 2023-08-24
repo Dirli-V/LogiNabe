9b97f53fccb74b3fd066e862b9aef03e6df87ba9
Dirli-V <github@dirli.net>
2023-08-24 20:03:17 +0200

    Setup project

--- flake.nix
+++ flake.nix
@@ -1,46 +1,53 @@
 {
   description = "Flake for LogiNabe";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    crane,
     flake-utils,
-    ...
+    nix-filter,
+    crane,
+    fenix,
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
+      pkgs = import nixpkgs {inherit system;};
+      craneLib = crane.lib.${system}.overrideToolchain fenix.packages.${system}.stable.toolchain;
+
+      pkgDef = {
+        src = nix-filter.lib.filter {
+          root = ./.;
+          include = [
+            ./src
+            ./Cargo.toml
+            ./Cargo.lock
+          ];
+        };
+        nativeBuildInputs = with pkgs; [pkg-config autoPatchelfHook];
+        buildInputs = [];
+        runtimeDependencies = [];
       };
 
-      craneLib = crane.lib.${system};
-      loginabe = craneLib.buildPackage {
-        src = craneLib.cleanCargoSource (craneLib.path ./.);
-
-        # buildInputs = [
-        #   # Add additional build inputs here
-        # ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-        #   # Additional darwin specific inputs can be set here
-        #   pkgs.libiconv
-        # ];
-
-        # Additional environment variables can be set directly
-        # MY_CUSTOM_VAR = "some value";
-      };
+      cargoArtifacts = craneLib.buildDepsOnly pkgDef;
+      loginabe = craneLib.buildPackage (pkgDef
+        // {
+          inherit cargoArtifacts;
+        });
     in {
       checks = {
-        inherit loginabe;
+        LogiNabe = loginabe;
       };
 
       packages.default = loginabe;
@@ -49,17 +56,14 @@
         drv = loginabe;
       };
 
-      devShells.default = pkgs.mkShell {
+      devShells.default = pkgs.mkShell rec {
         inputsFrom = builtins.attrValues self.checks.${system};
-
-        # Additional dev-shell environment variables can be set directly
-        # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-        # Extra inputs can be added here
-        # nativeBuildInputs = with pkgs; [
-        #   cargo
-        #   rustc
-        # ];
+        LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath (builtins.concatMap (d: d.runtimeDependencies) inputsFrom);
       };
     });
+
+  nixConfig = {
+    extra-trusted-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  };
 }
